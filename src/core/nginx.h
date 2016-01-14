6771862784569cc570591459a9248cee2873dd89
Christos Trochalakis <yatiohi@ideopolis.gr>
2016-01-14 10:06:28 +0200

    Imported Upstream version 1.9.9

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -9,8 +9,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1009006
-#define NGINX_VERSION      "1.9.6"
+#define nginx_version      1009009
+#define NGINX_VERSION      "1.9.9"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #ifdef NGX_BUILD
