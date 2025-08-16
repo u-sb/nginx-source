2f94780c1ccfc75fc6a260e6f58e96d4f9e74d71
Xinyue Lu <i@7086.in>
2025-08-16 16:58:50 -0700

    Dark Mode Support for Nginx Error Pages (From github:anvme)

--- src/http/ngx_http_special_response.c
+++ src/http/ngx_http_special_response.c
@@ -59,7 +59,7 @@ static u_char ngx_http_msie_refresh_tail[] =
 
 static char ngx_http_error_301_page[] =
 "<html>" CRLF
-"<head><title>301 Moved Permanently</title></head>" CRLF
+"<head><title>301 Moved Permanently</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>301 Moved Permanently</h1></center>" CRLF
 ;
@@ -67,7 +67,7 @@ static char ngx_http_error_301_page[] =
 
 static char ngx_http_error_302_page[] =
 "<html>" CRLF
-"<head><title>302 Found</title></head>" CRLF
+"<head><title>302 Found</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>302 Found</h1></center>" CRLF
 ;
@@ -75,7 +75,7 @@ static char ngx_http_error_302_page[] =
 
 static char ngx_http_error_303_page[] =
 "<html>" CRLF
-"<head><title>303 See Other</title></head>" CRLF
+"<head><title>303 See Other</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>303 See Other</h1></center>" CRLF
 ;
@@ -83,7 +83,7 @@ static char ngx_http_error_303_page[] =
 
 static char ngx_http_error_307_page[] =
 "<html>" CRLF
-"<head><title>307 Temporary Redirect</title></head>" CRLF
+"<head><title>307 Temporary Redirect</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>307 Temporary Redirect</h1></center>" CRLF
 ;
@@ -91,7 +91,7 @@ static char ngx_http_error_307_page[] =
 
 static char ngx_http_error_308_page[] =
 "<html>" CRLF
-"<head><title>308 Permanent Redirect</title></head>" CRLF
+"<head><title>308 Permanent Redirect</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>308 Permanent Redirect</h1></center>" CRLF
 ;
@@ -99,7 +99,7 @@ static char ngx_http_error_308_page[] =
 
 static char ngx_http_error_400_page[] =
 "<html>" CRLF
-"<head><title>400 Bad Request</title></head>" CRLF
+"<head><title>400 Bad Request</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>400 Bad Request</h1></center>" CRLF
 ;
@@ -107,7 +107,7 @@ static char ngx_http_error_400_page[] =
 
 static char ngx_http_error_401_page[] =
 "<html>" CRLF
-"<head><title>401 Authorization Required</title></head>" CRLF
+"<head><title>401 Authorization Required</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>401 Authorization Required</h1></center>" CRLF
 ;
@@ -115,7 +115,7 @@ static char ngx_http_error_401_page[] =
 
 static char ngx_http_error_402_page[] =
 "<html>" CRLF
-"<head><title>402 Payment Required</title></head>" CRLF
+"<head><title>402 Payment Required</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>402 Payment Required</h1></center>" CRLF
 ;
@@ -123,7 +123,7 @@ static char ngx_http_error_402_page[] =
 
 static char ngx_http_error_403_page[] =
 "<html>" CRLF
-"<head><title>403 Forbidden</title></head>" CRLF
+"<head><title>403 Forbidden</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>403 Forbidden</h1></center>" CRLF
 ;
@@ -131,7 +131,7 @@ static char ngx_http_error_403_page[] =
 
 static char ngx_http_error_404_page[] =
 "<html>" CRLF
-"<head><title>404 Not Found</title></head>" CRLF
+"<head><title>404 Not Found</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>404 Not Found</h1></center>" CRLF
 ;
@@ -139,7 +139,7 @@ static char ngx_http_error_404_page[] =
 
 static char ngx_http_error_405_page[] =
 "<html>" CRLF
-"<head><title>405 Not Allowed</title></head>" CRLF
+"<head><title>405 Not Allowed</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>405 Not Allowed</h1></center>" CRLF
 ;
@@ -147,7 +147,7 @@ static char ngx_http_error_405_page[] =
 
 static char ngx_http_error_406_page[] =
 "<html>" CRLF
-"<head><title>406 Not Acceptable</title></head>" CRLF
+"<head><title>406 Not Acceptable</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>406 Not Acceptable</h1></center>" CRLF
 ;
@@ -155,7 +155,7 @@ static char ngx_http_error_406_page[] =
 
 static char ngx_http_error_408_page[] =
 "<html>" CRLF
-"<head><title>408 Request Time-out</title></head>" CRLF
+"<head><title>408 Request Time-out</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>408 Request Time-out</h1></center>" CRLF
 ;
@@ -163,7 +163,7 @@ static char ngx_http_error_408_page[] =
 
 static char ngx_http_error_409_page[] =
 "<html>" CRLF
-"<head><title>409 Conflict</title></head>" CRLF
+"<head><title>409 Conflict</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>409 Conflict</h1></center>" CRLF
 ;
@@ -171,7 +171,7 @@ static char ngx_http_error_409_page[] =
 
 static char ngx_http_error_410_page[] =
 "<html>" CRLF
-"<head><title>410 Gone</title></head>" CRLF
+"<head><title>410 Gone</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>410 Gone</h1></center>" CRLF
 ;
@@ -179,7 +179,7 @@ static char ngx_http_error_410_page[] =
 
 static char ngx_http_error_411_page[] =
 "<html>" CRLF
-"<head><title>411 Length Required</title></head>" CRLF
+"<head><title>411 Length Required</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>411 Length Required</h1></center>" CRLF
 ;
@@ -187,7 +187,7 @@ static char ngx_http_error_411_page[] =
 
 static char ngx_http_error_412_page[] =
 "<html>" CRLF
-"<head><title>412 Precondition Failed</title></head>" CRLF
+"<head><title>412 Precondition Failed</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>412 Precondition Failed</h1></center>" CRLF
 ;
@@ -195,7 +195,7 @@ static char ngx_http_error_412_page[] =
 
 static char ngx_http_error_413_page[] =
 "<html>" CRLF
-"<head><title>413 Request Entity Too Large</title></head>" CRLF
+"<head><title>413 Request Entity Too Large</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>413 Request Entity Too Large</h1></center>" CRLF
 ;
@@ -203,7 +203,7 @@ static char ngx_http_error_413_page[] =
 
 static char ngx_http_error_414_page[] =
 "<html>" CRLF
-"<head><title>414 Request-URI Too Large</title></head>" CRLF
+"<head><title>414 Request-URI Too Large</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>414 Request-URI Too Large</h1></center>" CRLF
 ;
@@ -211,7 +211,7 @@ static char ngx_http_error_414_page[] =
 
 static char ngx_http_error_415_page[] =
 "<html>" CRLF
-"<head><title>415 Unsupported Media Type</title></head>" CRLF
+"<head><title>415 Unsupported Media Type</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>415 Unsupported Media Type</h1></center>" CRLF
 ;
@@ -219,7 +219,7 @@ static char ngx_http_error_415_page[] =
 
 static char ngx_http_error_416_page[] =
 "<html>" CRLF
-"<head><title>416 Requested Range Not Satisfiable</title></head>" CRLF
+"<head><title>416 Requested Range Not Satisfiable</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>416 Requested Range Not Satisfiable</h1></center>" CRLF
 ;
@@ -227,7 +227,7 @@ static char ngx_http_error_416_page[] =
 
 static char ngx_http_error_421_page[] =
 "<html>" CRLF
-"<head><title>421 Misdirected Request</title></head>" CRLF
+"<head><title>421 Misdirected Request</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>421 Misdirected Request</h1></center>" CRLF
 ;
@@ -235,7 +235,7 @@ static char ngx_http_error_421_page[] =
 
 static char ngx_http_error_429_page[] =
 "<html>" CRLF
-"<head><title>429 Too Many Requests</title></head>" CRLF
+"<head><title>429 Too Many Requests</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>429 Too Many Requests</h1></center>" CRLF
 ;
@@ -243,7 +243,7 @@ static char ngx_http_error_429_page[] =
 
 static char ngx_http_error_494_page[] =
 "<html>" CRLF
-"<head><title>400 Request Header Or Cookie Too Large</title></head>"
+"<head><title>400 Request Header Or Cookie Too Large</title><meta name=\"color-scheme\" content=\"light dark\"></head>"
 CRLF
 "<body>" CRLF
 "<center><h1>400 Bad Request</h1></center>" CRLF
@@ -253,7 +253,7 @@ CRLF
 
 static char ngx_http_error_495_page[] =
 "<html>" CRLF
-"<head><title>400 The SSL certificate error</title></head>"
+"<head><title>400 The SSL certificate error</title><meta name=\"color-scheme\" content=\"light dark\"></head>"
 CRLF
 "<body>" CRLF
 "<center><h1>400 Bad Request</h1></center>" CRLF
@@ -263,7 +263,7 @@ CRLF
 
 static char ngx_http_error_496_page[] =
 "<html>" CRLF
-"<head><title>400 No required SSL certificate was sent</title></head>"
+"<head><title>400 No required SSL certificate was sent</title><meta name=\"color-scheme\" content=\"light dark\"></head>"
 CRLF
 "<body>" CRLF
 "<center><h1>400 Bad Request</h1></center>" CRLF
@@ -273,7 +273,7 @@ CRLF
 
 static char ngx_http_error_497_page[] =
 "<html>" CRLF
-"<head><title>400 The plain HTTP request was sent to HTTPS port</title></head>"
+"<head><title>400 The plain HTTP request was sent to HTTPS port</title><meta name=\"color-scheme\" content=\"light dark\"></head>"
 CRLF
 "<body>" CRLF
 "<center><h1>400 Bad Request</h1></center>" CRLF
@@ -283,7 +283,7 @@ CRLF
 
 static char ngx_http_error_500_page[] =
 "<html>" CRLF
-"<head><title>500 Internal Server Error</title></head>" CRLF
+"<head><title>500 Internal Server Error</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>500 Internal Server Error</h1></center>" CRLF
 ;
@@ -291,7 +291,7 @@ static char ngx_http_error_500_page[] =
 
 static char ngx_http_error_501_page[] =
 "<html>" CRLF
-"<head><title>501 Not Implemented</title></head>" CRLF
+"<head><title>501 Not Implemented</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>501 Not Implemented</h1></center>" CRLF
 ;
@@ -299,7 +299,7 @@ static char ngx_http_error_501_page[] =
 
 static char ngx_http_error_502_page[] =
 "<html>" CRLF
-"<head><title>502 Bad Gateway</title></head>" CRLF
+"<head><title>502 Bad Gateway</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>502 Bad Gateway</h1></center>" CRLF
 ;
@@ -307,7 +307,7 @@ static char ngx_http_error_502_page[] =
 
 static char ngx_http_error_503_page[] =
 "<html>" CRLF
-"<head><title>503 Service Temporarily Unavailable</title></head>" CRLF
+"<head><title>503 Service Temporarily Unavailable</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>503 Service Temporarily Unavailable</h1></center>" CRLF
 ;
@@ -315,7 +315,7 @@ static char ngx_http_error_503_page[] =
 
 static char ngx_http_error_504_page[] =
 "<html>" CRLF
-"<head><title>504 Gateway Time-out</title></head>" CRLF
+"<head><title>504 Gateway Time-out</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>504 Gateway Time-out</h1></center>" CRLF
 ;
@@ -323,7 +323,7 @@ static char ngx_http_error_504_page[] =
 
 static char ngx_http_error_505_page[] =
 "<html>" CRLF
-"<head><title>505 HTTP Version Not Supported</title></head>" CRLF
+"<head><title>505 HTTP Version Not Supported</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>505 HTTP Version Not Supported</h1></center>" CRLF
 ;
@@ -331,7 +331,7 @@ static char ngx_http_error_505_page[] =
 
 static char ngx_http_error_507_page[] =
 "<html>" CRLF
-"<head><title>507 Insufficient Storage</title></head>" CRLF
+"<head><title>507 Insufficient Storage</title><meta name=\"color-scheme\" content=\"light dark\"></head>" CRLF
 "<body>" CRLF
 "<center><h1>507 Insufficient Storage</h1></center>" CRLF
 ;
